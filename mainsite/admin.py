9972d3be2034ef6e4eddeae834110cc660e8c57c
YOUNGGIT88 <young@butyshop.com>
2021-08-04 07:18:48 -0700

    2021_08_04

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+class Postadmin(admin.ModelAdmin):
+	list_display=('title','slug','pub_date') #COLUMN DISPLAY ON ADMIN
+	
+admin.site.register(Post,Postadmin)
--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Post
 # Create your views here.
+def homepage(request):
+	posts=Post.objects.all()
+	post_lists=list()
+	for count,post in enumerate(posts):
+		post_lists.append("NO.{}:".format(str(count))+str(post)+"<hr>")
+		post_lists.append("<small>"+str(post.body.encode('utf-8'))+"</small><br><br>")
+		
+	return HttpResponse(post_lists)
