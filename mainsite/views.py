d665237eb16e22bde484645bd2d0a2664352d1eb
young <young@butyshop.com>
2021-08-05 01:44:03 +0800

    20210805

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from datetime import datetime
 # Create your views here.
 def homepage(request):
 	posts=Post.objects.all()
+	now=datetime.now()
 	post_lists=list()
 	for count,post in enumerate(posts):
 		post_lists.append("NO.{}:".format(str(count))+str(post)+"<hr>")
 		post_lists.append("<small>"+str(post.body.encode('utf-8'))+"</small><br><br>")
-		
-	return HttpResponse(post_lists)
+	return render(request,'index.html',locals())
