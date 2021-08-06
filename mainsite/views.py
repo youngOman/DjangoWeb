442d788cfd554127df071b81ec8c0fa708e9ee8e
young <young@butyshop.com>
2021-08-07 01:51:10 +0800

    20210807

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import Post
 from datetime import datetime
@@ -11,3 +11,11 @@ def homepage(request):
 		post_lists.append("NO.{}:".format(str(count))+str(post)+"<hr>")
 		post_lists.append("<small>"+str(post.body.encode('utf-8'))+"</small><br><br>")
 	return render(request,'index.html',locals())
+	
+def showpost(request,slug):
+	try: 
+		post=Post.objects.get(slug=slug)
+		if post!=None:
+			return render(request,'post.html',locals())
+	except:
+		return redirect("/")
