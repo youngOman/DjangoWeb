b020bf6542cea8bdb5ddb3d3268d7c0195cc32d7
young <young@butyshop.com>
2021-08-08 20:32:23 +0800

    20210808_8pm templates modified

--- mblog/urls.py
+++ mblog/urls.py
@@ -1,5 +1,4 @@
 """mblog URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
@@ -21,5 +20,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),    
     path('',homepage),
     path('post/<slug:slug>/',showpost),
+    path('accounts/', include('django.contrib.auth.urls')),
+
 ]
 
+ 
